19d0d4247c35ef34c8de88d80f3a9bf3fc78de0f
Lurux <35870988+Lurux@users.noreply.github.com>
2020-06-04 19:58:24 +0200

    Add files via upload
    Upload missing software.js

---
+++ software.js
@@ -0,0 +1,19 @@
+function showall() {
+	contents = document.querySelectorAll(".software-grid > a");
+
+	for(i = 0; i < contents.length; i++) {
+		contents[i].classList.remove("hidden");
+	}
+}
+
+function showtype(type) {
+	contents = document.querySelectorAll(".software-grid > a");
+
+	for(i = 0; i < contents.length; i++) {
+		if(contents[i].dataset.fields.charAt(type) == 0) {
+			contents[i].classList.add("hidden");
+		} else {
+			contents[i].classList.remove("hidden");
+		}
+	}
+}
